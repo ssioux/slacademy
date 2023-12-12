c7ab231cc921e4dd47788f8af8e7f8e15da98857
David Lazaro <dlazaro7@gmail.com>
2023-12-12 12:29:20 +0100

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import logo from "./assets/images/palmBigAlone.png"
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
+    <NavBar/>
       <section className="header">
         <img src={logo} alt="" />
         <h1>SLAcademy</h1>
