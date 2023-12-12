77bddd01197e9c57c76a57cc246f303cb947c194
David Lazaro <dlazaro7@gmail.com>
2023-12-12 11:56:20 +0100

    logo imported

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import logo from "./assets/images/palmBigAlone.png"
 
 function App() {
   return (
     <>
       <section className="header">
-        <img src="/src/assets/images/palmBigAlone.png" alt="" />
+        <img src={logo} alt="" />
         <h1>SLAcademy</h1>
       </section>
     </>
