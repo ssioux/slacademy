f5db4e7851da212a6bef9d595372e035f5589ae6
David Lazaro <dlazaro7@gmail.com>
2023-12-12 11:49:29 +0100

    logo+favicon added

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import "./App.css";
 function App() {
   return (
     <>
-   
+      <section className="header">
+        <img src="/src/assets/images/palmBigAlone.png" alt="" />
         <h1>SLAcademy</h1>
-    
+      </section>
     </>
   );
 }
