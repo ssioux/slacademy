2abf1a68f5eeb70cfdbec8b014343fe00ec68fb1
David Lazaro <dlazaro7@gmail.com>
2023-12-12 11:52:07 +0100

    vite-lint cfg settings added

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,21 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: [
+        {
+            // this is required for the SCSS modules
+            find: /^~(.*)$/,
+            replacement: '$1',
+        },
+    ],
+},
   plugins: [react()],
+  server: {
+    port: 3000,
+    watch: {
+      usePolling: true,
+    },
+    hmr: { overlay: false },
+  },
 })
