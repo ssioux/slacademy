086fc31aa12e9937949608a1e6caaa5d2f0776ac
David Lazaro <dlazaro7@gmail.com>
2023-12-15 10:51:00 +0100

    carousel updated to MDB

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,53 +1,46 @@
-import Carousel from 'react-bootstrap/Carousel';
+import { MDBCarousel, MDBCarouselItem } from "mdb-react-ui-kit";
 import caro1 from "../../assets/images/carousel/caro1.jpg";
 import caro2 from "../../assets/images/carousel/caro2.jpeg";
 import caro3 from "../../assets/images/carousel/caro3.jpg";
 
-import './home.scss';
+import "./home.scss";
 function Home() {
   return (
     <>
       <section className="header">
-
-        <div className='carousel'>
-        <Carousel data-bs-theme="dark" className='caro'>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={caro1}
-          alt="siquijor_pic_1"
-        />
-
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={caro2}
-          alt="siquijor_pic_2"
-        />
-  
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={caro3}
-          alt="siquijor_pic_3"
-        />
-      </Carousel.Item>
-    </Carousel>
+        <div className="carousel">
+          <MDBCarousel showControls showIndicators>
+            <MDBCarouselItem itemId={1}>
+              <img src={caro1} className="d-block w-100" alt="..." />
+            </MDBCarouselItem>
+            <MDBCarouselItem itemId={2}>
+              <img src={caro2} className="d-block w-100" alt="..." />
+            </MDBCarouselItem>
+            <MDBCarouselItem itemId={3}>
+              <img src={caro3} className="d-block w-100" alt="..." />
+            </MDBCarouselItem>
+          </MDBCarousel>
         </div>
-        <div className="intro">        
-        <h2>Quieres estudiar inglés en Filipinas y además disfrutar en una isla paradisíaca.</h2>
+        <div className="intro">
+          <h2>
+            Quieres estudiar inglés en Filipinas y además disfrutar en una isla
+            paradisíaca.
+          </h2>
 
-        <p>En Siquijor Language Academy queremos ofrecerte la oportunidad de explorar la misteriosa isla de Siquijor, Filipinas,
-           mientras aprendes inglés y obtienes el título TESOL/TELF. Además, podrás disfrutar de las maravillas de esta isla paradisíaca, 
-           como bucear en los mejores arrecifes, explorar cuevas impresionantes y relajarte en playas de arena blanca. 
-           Nuestro objetivo es hacer que tu viaje sea lo más sencillo y agradable posible. 
-           Por eso, desde Siquijor Language Academy te brindaremos todo el apoyo necesario para que solo tengas que disfrutar. 
-           No dudes en contar con nuestra ayuda para hacer realidad este emocionante viaje que estás a punto de emprender.</p>
-
+          <p>
+            En Siquijor Language Academy queremos ofrecerte la oportunidad de
+            explorar la misteriosa isla de Siquijor, Filipinas, mientras
+            aprendes inglés y obtienes el título TESOL/TELF. Además, podrás
+            disfrutar de las maravillas de esta isla paradisíaca, como bucear en
+            los mejores arrecifes, explorar cuevas impresionantes y relajarte en
+            playas de arena blanca. Nuestro objetivo es hacer que tu viaje sea
+            lo más sencillo y agradable posible. Por eso, desde Siquijor
+            Language Academy te brindaremos todo el apoyo necesario para que
+            solo tengas que disfrutar. No dudes en contar con nuestra ayuda para
+            hacer realidad este emocionante viaje que estás a punto de
+            emprender.
+          </p>
         </div>
-
       </section>
     </>
   );
